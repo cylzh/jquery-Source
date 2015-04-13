d01e21a7aa232a2f0d5d1eee06ecccd7bd653907
ddsoma <yuliangchenxc@163.com>
2015-04-13 08:19:54 +0800

    no message

--- 整体框架/aQuery.js
+++ 整体框架/aQuery.js
@@ -1,9 +1,12 @@
 var aQuery=function(){
-
+    return aQuery.prototype.init();
 }
 
 
-aQuery().prototype={
+aQuery.prototype={
+    init:function(){
+      return this;
+    },
     name:function(){},
     age:function(){
 
